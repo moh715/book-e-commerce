f4174607dcbc511a329b3b7977edae0fba62c1c6
MohamedElghali <mohamed.elghalii01@gmail.com>
2026-05-08 06:41:29 +0300

    Project Setup

---
+++ src/main/java/com/ecommerce/ecomgrade/EcomgradeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecomgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomgradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomgradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/ecomgrade/EcomgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecomgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomgradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
